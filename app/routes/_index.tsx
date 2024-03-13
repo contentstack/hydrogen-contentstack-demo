3ec9d20eb4b61bc8f7a6ffec94f97c0902d9d591
ChinmayeeMestry <chinmayee.mestry@contentstack.com>
2024-03-13 22:24:23 +0530

    added changes in UI

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,11 @@
 import {defer, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
 import {Await, useLoaderData, Link, type MetaFunction} from '@remix-run/react';
 import {Fragment, Suspense} from 'react';
-import {Image, Money} from '@shopify/hydrogen';
-import type {RecommendedProductsQuery} from 'storefrontapi.generated';
-import banner from '../../public/banner.svg';
+import {getPaginationVariables, Image, Money} from '@shopify/hydrogen';
+import type {
+  CollectionFragment,
+  RecommendedProductsQuery,
+} from 'storefrontapi.generated';
 import '../styles/pages.css';
 import {getEntryByUid} from '~/components/contentstack-sdk';
 
@@ -11,7 +13,7 @@ export const meta: MetaFunction = () => {
   return [{title: 'Hydrogen | Home'}];
 };
 
-export async function loader({context}: LoaderFunctionArgs) {
+export async function loader({context, request}: LoaderFunctionArgs) {
   const {storefront} = context;
   const recommendedProducts = storefront.query(RECOMMENDED_PRODUCTS_QUERY);
   const envConfig = context.env;
@@ -28,9 +30,18 @@ export async function loader({context}: LoaderFunctionArgs) {
       console.error('ERROR', error);
     }
   };
+  const paginationVariables = getPaginationVariables(request, {
+    pageBy: 6,
+  });
+
+  const {collections} = await context.storefront.query(COLLECTIONS_QUERY, {
+    variables: paginationVariables,
+  });
+
   return defer({
     recommendedProducts,
     fetchedData: await fetchData(),
+    collections,
   });
 }
 
@@ -41,17 +52,48 @@ export default function Homepage() {
       <RecommendedProducts
         products={data.recommendedProducts}
         cmsData={data.fetchedData}
+        collections={data.collections.nodes}
       />
     </div>
+  );
+}
+
+function CollectionItem({
+  collection,
+  index,
+}: {
+  collection: CollectionFragment;
+  index: number;
+}) {
+  return (
+    <Link
+      className="recommended-product flex"
+      key={collection.id}
+      to={`/collections/${collection.handle}`}
+      prefetch="intent"
+    >
+      {collection?.image && (
+        <Image
+          alt={collection.image.altText || collection.title}
+          aspectRatio="1/1"
+          data={collection.image}
+          loading={index < 3 ? 'eager' : undefined}
+          className="collection_image"
+        />
+      )}
+      <p className="product_cta collection_content">{collection.title}</p>
+    </Link>
   );
 }
 
 function RecommendedProducts({
   products,
   cmsData,
+  collections,
 }: {
   products: Promise<RecommendedProductsQuery>;
   cmsData: Awaited<ReturnType<typeof getEntryByUid>>;
+  collections: CollectionFragment[];
 }) {
   return (
     <div>
@@ -59,9 +101,9 @@ function RecommendedProducts({
         <h1 className="page_banner_content bodyCss">{cmsData?.banner_title}</h1>
         <div className="flex gap">
           <a
-            href={cmsData.button.repo.cta_title.href}
+            href={cmsData?.button?.repo?.cta_title?.href}
             rel="noreferrer"
-            target={cmsData.button.repo.open_in_new_tab && '_blank'}
+            target={cmsData.button.repo.open_in_new_tab ? '_blank' : '_self'}
             className="banner_repo_cta"
           >
             {cmsData.button.repo.cta_title.title}
@@ -69,7 +111,9 @@ function RecommendedProducts({
           <a
             href={cmsData.button.products.cta_title.href}
             rel="noreferrer"
-            target={cmsData.button.products.open_in_new_tab && '_blank'}
+            target={
+              cmsData.button.products.open_in_new_tab ? '_blank' : '_self'
+            }
             className="banner_repo_cta"
           >
             {cmsData.button.products.cta_title.title}
@@ -80,51 +124,71 @@ function RecommendedProducts({
         <Await resolve={products}>
           {({products}) => {
             return (
-              <div className="featured_wrapper container">
-                <div className="featuredContent">
-                  <h2 className="bodyCss">{cmsData.feature_title}</h2>
+              <div>
+                <div className="featured_wrapper container">
+                  <div className="featuredContent">
+                    <h2 className="bodyCss">{cmsData.feature_title}</h2>
+                  </div>
+                  <div className="recommended-products-grid">
+                    {products.nodes.map((product) => {
+                      return (
+                        <Fragment key={product.id}>
+                          {!product.images.nodes[0] ||
+                            (product.images.nodes[0] != null && (
+                              <Link
+                                className="recommended-product"
+                                to={`/products/${product.handle}`}
+                              >
+                                {product.images.nodes[0] && (
+                                  <Image
+                                    data={product.images.nodes[0]}
+                                    aspectRatio="1/1.2"
+                                    sizes="(min-width: 45em) 20vw, 50vw"
+                                  />
+                                )}
+                                <p className="product_cta">{product.title}</p>
+                                <small>
+                                  <Money
+                                    className="product_price"
+                                    data={product.priceRange.minVariantPrice}
+                                  />
+                                </small>
+                              </Link>
+                            ))}
+                        </Fragment>
+                      );
+                    })}
+                  </div>
+                  <div className="center">
+                    <Link
+                      to={cmsData.view_all_product.cta_title.href}
+                      rel="noreferrer"
+                      target={
+                        cmsData.view_all_product.open_in_new_tab && '_blank'
+                      }
+                      className="view_allproducts"
+                    >
+                      {cmsData.view_all_product.cta_title.title}
+                    </Link>
+                  </div>
                 </div>
-                <div className="recommended-products-grid">
-                  {products.nodes.map((product) => {
-                    return (
-                      <Fragment key={product.id}>
-                        {!product.images.nodes[0] ||
-                          (product.images.nodes[0] != null && (
-                            <Link
-                              className="recommended-product"
-                              to={`/products/${product.handle}`}
-                            >
-                              {product.images.nodes[0] && (
-                                <Image
-                                  data={product.images.nodes[0]}
-                                  aspectRatio="1/1"
-                                  sizes="(min-width: 45em) 20vw, 50vw"
-                                />
-                              )}
-                              <p className="product_cta">{product.title}</p>
-                              <small>
-                                <Money
-                                  className="product_price"
-                                  data={product.priceRange.minVariantPrice}
-                                />
-                              </small>
-                            </Link>
-                          ))}
-                      </Fragment>
-                    );
-                  })}
-                </div>
-                <div className="center">
-                  <Link
-                    to={cmsData.view_all_product.cta_title.href}
-                    rel="noreferrer"
-                    target={
-                      cmsData.view_all_product.open_in_new_tab && '_blank'
-                    }
-                    className="view_allproducts"
-                  >
-                    {cmsData.view_all_product.cta_title.title}
-                  </Link>
+                <div className="collection_wrapper featured_wrapper">
+                  <div className="container">
+                    {/* <div className="featuredContent"> */}
+                    <h2 className="bodyCss collection_heading">
+                      {cmsData.collection_heading}
+                    </h2>
+                    {/* </div> */}
+                    <div className="recommended-products-grid container mg_0">
+                      {collections.map((collection, index) => (
+                        <CollectionItem
+                          key={collection.id}
+                          collection={collection}
+                          index={index}
+                        />
+                      ))}
+                    </div>
+                  </div>
                 </div>
               </div>
             );
@@ -182,11 +246,51 @@ const RECOMMENDED_PRODUCTS_QUERY = `#graphql
   }
   query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
     @inContext(country: $country, language: $language) {
-    products(first: 9, sortKey: UPDATED_AT, reverse: true) {
+    products(first: 3, sortKey: UPDATED_AT, reverse: true) {
       nodes {
         ...RecommendedProduct
       }
   
+    }
+  }
+` as const;
+const COLLECTIONS_QUERY = `#graphql
+  fragment Collection on Collection {
+    id
+    title
+    handle
+    description
+    image {
+      id
+      url
+      altText
+      width
+      height
+    }
+  }
+  query StoreCollections(
+    $country: CountryCode
+    $endCursor: String
+    # $first: Int
+    $language: LanguageCode
+    $last: Int
+    $startCursor: String
+  ) @inContext(country: $country, language: $language) {
+    collections(
+      first: 3,
+      last: $last,
+      before: $startCursor,
+      after: $endCursor
+    ) {
+      nodes {
+        ...Collection
+      }
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
     }
   }
 ` as const;
